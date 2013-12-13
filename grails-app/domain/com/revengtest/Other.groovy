4e3c824fe4ee8068254c350d92813f652e068a32
kknd22 <chris.lin@mail.com>
2013-12-13 09:29:04 -0500

    added war

--- grails-app/domain/com/revengtest/Other.groovy
+++ grails-app/domain/com/revengtest/Other.groovy
@@ -3,7 +3,11 @@ package com.revengtest
 class Other {
 
 	String username
-
+	
+	static transients = ['id']
+	void setId(String id) { username = id }
+	String getId() { return username}
+	
 	static mapping = {
 		id name: "username", generator: "assigned"
 		version "nonstandard_version_name"
